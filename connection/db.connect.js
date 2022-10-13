3faa2c997aecfacac2f0233aa7d5d274dafd3997
Franklin P. Thaker <Jarvisfranklinthaker@gmail.com>
2022-10-13 17:23:30 +0530

    Trying: to fix the workflow - test 7

--- connection/db.connect.js
+++ connection/db.connect.js
@@ -1,4 +1,3 @@
-const { MongoMemoryServer } = require('mongodb-memory-server');
 const mongoose = require('mongoose');
 const { envConstants } = require('../helpers/constants');
 
@@ -15,19 +14,14 @@ const connectionObj = {
 
 if (envConstants.AUTHENTICATION === 'false') delete connectionObj.auth;
 
-let mongoServer;
-
 exports.connect = async () => {
   mongoose.Promise = global.Promise;
   mongoose.set('debug', envConstants.DB_DEBUG_MODE === 'true');
   try {
-    if (process.env.NODE_ENV !== 'test') {
-      await mongoose.connect(`${envConstants.DB_DIALECT}://${envConstants.DB_HOST}:${envConstants.DB_PORT}/${envConstants.DB_NAME}`, connectionObj);
+    if (process.env.NODE_ENV === 'test') {
+      await mongoose.connect(`${envConstants.DB_DIALECT}://${envConstants.DB_HOST}:${envConstants.DB_PORT}/test_db`, connectionObj);
     } else {
-      mongoose.set('useFindAndModify', false);
-      mongoServer = await MongoMemoryServer.create();
-      const uri = await mongoServer.getUri();
-      await mongoose.connect(uri, connectionObj);
+      await mongoose.connect(`${envConstants.DB_DIALECT}://${envConstants.DB_HOST}:${envConstants.DB_PORT}/${envConstants.DB_NAME}`, connectionObj);
     }
   } catch (error) {
     process.exit(1);
@@ -37,15 +31,7 @@ exports.connect = async () => {
 exports.removeDB = async () => {
   await mongoose.connection.dropDatabase();
   await mongoose.connection.close();
-  if (mongoServer) await mongoServer.stop();
 };
-
-/* exports.removeDB = async () => {
-  const { connection } = mongoose;
-  connection.once('open', async () => {
-    mongoose.connection.db.dropDatabase(connection.db.databaseName);
-  });
-}; */
 
 /* exports.disconnect = async () => {
   mongoose.Promise = global.Promise;
