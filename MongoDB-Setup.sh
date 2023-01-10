f191ab03352ce0ede6eb499b8c2a4b461ba8df94
Franklin P. Thaker <jarvisfranklinthaker@gmail.com>
2023-01-10 17:45:36 +0530

    Update MongoDB-Setup.sh

--- MongoDB-Setup.sh
+++ MongoDB-Setup.sh
@@ -1,6 +1,7 @@
 sudo apt-get install gnupg
 wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc | sudo apt-key add -
-echo "deb http://repo.mongodb.org/apt/debian buster/mongodb-org/6.0 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
+# this is old -> echo "deb http://repo.mongodb.org/apt/debian buster/mongodb-org/6.0 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
 sudo apt-get update
 sudo apt-get install -y mongodb-org
 sudo service mongod start
